c3412ae3971347aa7dd97b2fac2c9b569ed30858
robson-dot <robson.correia@dotgroup.com.br>
2020-01-16 19:18:58 -0300

    iniciando front end web

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
