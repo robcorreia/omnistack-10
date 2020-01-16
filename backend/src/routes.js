35593149d23505d05a6af43e1a615f64d2f58859
robson-dot <robson.correia@dotgroup.com.br>
2020-01-15 22:19:30 -0300

    separando o controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,44 +1,11 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 
 const routes = Router();
 
 
-routes.get('/devs', (req, res) => {
-  return res.json();
-})
-
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs, latitude, longitude } = req.body;
-
-  const response = await axios.get(`https://api.github.com/users/${github_username}`);
-
-  const { name = login, avatar_url, bio } = response.data;
-
-  //pega a string e transforma em array quebrando pela ',' depois
-  //com map percorre o array e pra cada tech ele limpa os espaços em branco
-  // console.log(techs.split(',').map(tech => tech.trim()));
-  const techsArray = techs.split(',').map(tech => tech.trim());
-
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  }
-
-  const dev = await Dev.create({
-    github_username : github_username,
-    name,
-    avatar_url,
-    bio,
-    techs : techsArray,
-    location,
-  })
-
-  return res.json(dev);
-
-})
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
 
